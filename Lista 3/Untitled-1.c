590bf8f585ded91527fdbdf115793dbe8fcb78b1
Luigi Dovera <luigidovera11@gmail.com>
2020-10-03 18:06:11 -0300

    problema no Compara

--- Lista 3/Untitled-1.c
+++ Lista 3/Untitled-1.c
@@ -1,91 +1,119 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int compara(int tipo, void *arg1, void *arg2){
-    if(tipo==1){
-        if( *((int*)arg1) <  *((int*)arg2) ){
+int comparaInt(void *arg1, void *arg2){
+    printf("aa");
+    if( *((int*)arg1) <  *((int*)arg2) ){
             return 0;
         }else if( *((int*)arg1) == *((int*)arg2) ){
             return 1;
         }else{
             return 2;
         }
-    }else if(tipo==2){
-        if( *((float*)arg1) <  *((float*)arg2) ){
+}
+
+int comparaFloat(void *arg1, void *arg2){
+    
+    if( *((float*)arg1) <  *((float*)arg2) ){
             return 0;
         }else if( *((float*)arg1) == *((float*)arg2) ){
             return 1;
         }else{
             return 2;
         }
-    }
+}
+
+void recebeInt(void *arg1, void*arg2){
+    *((int*)arg1) = *((int*)arg2);
+}
+
+void recebeFloat(void *arg1, void*arg2){
+    *((float*)arg1) = *((float*)arg1);
+}
+
+void alocaInt(void *arg1){
+    arg1 = (int *) malloc(sizeof(int *));
+}
+
+void alocaFloat(void *arg1){
+    arg1 = (float *) malloc(sizeof(float *));
 }
 
 
-
-
-void recebe(int tipo, void* arg1, void *arg2){
-    if (tipo==1){
-        *((int*)arg1) = *((int*)arg2);
-    }else if(tipo==2){
-        *((float*)arg1) = *((float*)arg1)
-    }
-}
-
-
-
-void quicksort (void **vetor, int esq, int dir, int tipo, int (*compara)(void*, void*)){
-    int i, j;
+void quicksort (void **vetor, int esq, int dir,
+        int (*compara)(void*, void*), void (*recebe)(void*, void*),
+        void (*aloca)(void *)){
+    int i, j, z;
     void *x, *y;
+    aloca(x);
+    aloca(y);
 
-
+    printf("aa");
     i = esq;
     j = dir;
-    recebe(x = vetor[(esq + dir)/2];
+    z = (int) floor((esq + dir)/2);
+    recebe(x, vetor[z]);
 
     while(i<=j){
-        while (compara(tipo, vetor[i], &x) == 0 && i < dir){
+        while (compara(vetor[i], x) == 0 && i < dir){
             i++;
         }
-        while (compara(tipo, vetor[j], &x) == 2 && j > esq){
+        while (compara(vetor[j], x) == 2 && j > esq){
             j++;
         }
-        
+        printf("aa");
         if(i<=j){
-            y = vetor[i];
-            vetor[i] = vetor[j];
-            vetor[j] = y;
+            recebe(y, vetor[i]);
+            recebe(vetor[i], vetor[j]);
+            recebe(vetor[j], y);
             i++;
             j--;
         }
     }
 
     if (j>esq){
-        quicksort(vetor, esq, j, tipo, compara);
+        quicksort(vetor, esq, j, compara, recebe, aloca);
     }
     if (i<dir){
-        quicksort(vetor, i, dir, tipo, compara);
+        quicksort(vetor, i, dir, compara, recebe, aloca);
     }
 
 
 }
 
 
+/*
 int a(int (*compara)(int, void*, void*)){
     int a=2, b=2;
     void *pa=&a, *pb=&b;
     return compara(1, pa, pb);
 }
+*/
 
 int main(){
 
     
     int tipo, n;
     void **v;
+    int (*comp)(void *, void  *);
+    void (*rec)(void *, void  *);
+    void (*aloc)(void *);
 
     
     printf("1-Inteiro 2-Real\n");
     scanf("%d", &tipo);
+
+    if(tipo==1){
+        comp = comparaInt;
+        rec = recebeInt;
+        aloc = alocaInt;
+    }else{
+        comp = comparaFloat;
+        rec = recebeFloat;
+        aloc = alocaFloat;
+    }
+
     printf("Tamanho de n?\n");
     scanf("%d", &n);
     
@@ -104,13 +132,13 @@ int main(){
     *((int*)v[2]) = 0;
     *((int*)v[3]) = 2;
 
-    quicksort(v, 0, n, tipo, compara);
-
-    /*
+    quicksort(v, 0, n-1, comp, rec, aloc);
+    
+    printf("a");
     for(int i=0; i<n; i++){
         printf("%d", *((int*)v[i]));
     }
-    */
+    
 
     //int a=1, b=2;
     //void *pa=&a, *pb=&b;
