f2da428d0e9b5a45aee59f4691d1cec836b597d8
Luigi Dovera <luigidovera11@gmail.com>
2021-01-06 14:05:56 -0300

    finalizando ed (de novo)

---
+++ Daniel_Pinto_e_Italo_Luigi_Trabalho_de_Implementacao_03_EDI/Daniel_Pinto_e_Italo_Luigi_Trabalho_de_Implementacao_03_EDI/danielpintoitaloluigiEDITrabalhoDeImplementacao03/bstree.h
@@ -0,0 +1,25 @@
+#ifndef BSTREE_H
+#define BSTREE_H
+
+#define TRUE 1
+#define FALSE 0
+
+typedef struct _tnode_ TNode;
+typedef struct _tree_ Tree;
+
+TNode *bstCreate(TNode *t, void *data);
+
+void bstPreOrder(TNode *t, void (*visit)(void *));
+void bstInOrder(TNode *t, void (*visit)(void *));
+void bstPostOrder(TNode *t, void (*visit)(void *));
+
+TNode *bstInsert(TNode *t, void *data, int (*cmp)(void*, void *));
+void *bstQuery(TNode *t, void *key, int (*cmp)(void *, void *));
+TNode *bstRemoveSmallest(TNode *t, void *key, int (*cmp)(void *, void *), void **data);
+TNode *bstRemove(TNode *t, void *key, int (*cmp)(void *, void *), void **data);
+
+void *bstGreaterNode(TNode *t);
+
+void *bstPurge(TNode *t);
+
+#endif
